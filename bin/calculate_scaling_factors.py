Implemented fix for issue with scaling factors when registering

bin/calculate_scaling_factors.py
@@ -21,9 +21,10 @@ if __name__ == "__main__":
     parser.add_argument('-i','--input', help='Base dir to input organized <plate>/<row>/<col>/<field>.ome.tiff', required=True)
     parser.add_argument('-o','--output', help='Output folder', default="./")
     parser.add_argument('-p','--plate', help='Subfolders to process', nargs='+', default=None)
+    parser.add_argument('--plate_groups', help='File describing how plates are grouped, used for multicycle runs to ensure scaling is done per cycles. Use registration_manifest.tsv from tglow pipeline', default=None)
     parser.add_argument('--blacklist', help='TSV file with "<plate>  <well>" on each row descrbing what to ignore', default=None)
-    parser.add_argument('--q1', help='Quantile 1, the quantile in an image', default="q99.9")
-    parser.add_argument('--q2', help='Quantile 2, the quantile over all images in input 0-100', default="99")
+    parser.add_argument('--q1', help='Quantile 1, the quantile in an image', default="q99.9999")
+    parser.add_argument('--q2', help='Quantile 2, the quantile over all images in input 0-100', default="95")
     parser.add_argument('--pattern', help='File pattern of sumstats tsv', default="intensity_stats.tsv")
     parser.add_argument('--scale_max', help='The max possible value of the input images, this will be the max value of the output', default=65535)
     args = parser.parse_args()
@@ -55,9 +56,54 @@ if __name__ == "__main__":
 
 
     main_df = None
+
+    indices = []
+    scale_factors = []
+    stats_max = []
+    stats_mean = []
+    stats_median = []
+    quantiles = [0, 0.01, 0.1, 25, 50, 75, 95, 99, 99.9, 99.99, 99.999, 99.9999, 100]
     
+    plate_groups = []
+    
+    # Define the plate groups (one cycle of imaging = a plate group)
+    # This ensures channels are scaled witin a cycle and not accross cycles.
+    if args.plate_groups != None:
+        df = pd.read_csv(args.plate_groups, sep="\t")
+        
+        print(df.head())
+                
+        # Extract the first column (ref plate)
+        plate_groups.append(df["reference_plate"].tolist())
+
+        # Extract the third column (query plates)
+        qry_plates = df["query_plates"].tolist()
+
+        # Split the 3rd col on comma, and add each subsequent col
+        # as a plategroup
+        i = 0
+        for qry in qry_plates:
+            cur_qry = qry.split(",") 
+            
+            for j in range(0, len(cur_qry)):
+                if i == 0:
+                    plate_groups.append([])
+                    
+                plate_groups[j+1].append(cur_qry[j])
+        
+            i += 1
+    
+    else:
+        plate_groups = [[main_df["plate"].unique()]]
+    
+    log.info("Considering plate groups:")
+    log.info(plate_groups)
+    
+    
+    log.info("Reading intensity files")
+        # Read per well intensity CSV files
     for file in files:
-        log.debug(f"Reading: {file}")
+        #log.debug(f"Reading: {file}")
         cur_df = pd.read_csv(file, sep="\t")
         
         if main_df is not None:
@@ -65,61 +111,71 @@ if __name__ == "__main__":
         else:
             main_df = cur_df
 
-    channels = main_df["channel"].unique()
 
-    scale_factors = []
-    stats_max = []
-    stats_mean = []
-    stats_median = []
-    
-    quantiles = [0, 0.01, 0.1, 25, 50, 75, 99, 99.9, 100]
+    i = 0
+    for plate_group in plate_groups:
+        # Subset to the plates in the group to normalize
+        plate_df = main_df[main_df["plate"].isin(plate_group)]
 
-    for channel in channels: 
-        cur_df = main_df[main_df["channel"] == channel]
-        scale_factor = np.percentile(cur_df[args.q1], args.q2) / args.scale_max
+        # Find available channels
+        channels = plate_df["channel"].unique()
+
+        for channel in channels: 
+            
+            indices.append(f"group{i}_ch{channel}")
         
-        # Max
-        cur_stats = np.percentile(cur_df["q100"], quantiles).tolist()
-        cur_stats.append(np.mean(cur_df["q100"]))
-        stats_max.append(cur_stats)
+            # Subset to the current channel
+            cur_df = plate_df[plate_df["channel"] == channel]
         
-        # Mean
-        cur_stats = np.percentile(cur_df["mean"], quantiles).tolist()
-        cur_stats.append(np.mean(cur_df["mean"]))
-        stats_mean.append(cur_stats)
-        
-        # Median
-        cur_stats = np.percentile(cur_df["q50"], quantiles).tolist()
-        cur_stats.append(np.mean(cur_df["q50"]))
-        stats_median.append(cur_stats)
-        
-        # Scale factors
-        for plate in main_df["plate"].unique():
-            scale_factors.append(f"{plate}_ch{channel}={scale_factor}")
+            # Calculate the scale factor
+            scale_factor = np.percentile(cur_df[args.q1], args.q2) / args.scale_max
+            
+            # Max
+            cur_stats = np.percentile(cur_df["q100"], quantiles).tolist()
+            cur_stats.append(np.mean(cur_df["q100"]))
+            stats_max.append(cur_stats)
+            
+            # Mean
+            cur_stats = np.percentile(cur_df["mean"], quantiles).tolist()
+            cur_stats.append(np.mean(cur_df["mean"]))
+            stats_mean.append(cur_stats)
+            
+            # Median
+            cur_stats = np.percentile(cur_df["q50"], quantiles).tolist()
+            cur_stats.append(np.mean(cur_df["q50"]))
+            stats_median.append(cur_stats)
+            
+            # Scale factors
+            for plate in plate_df["plate"].unique():
+                scale_factors.append(f"{plate}_ch{channel}={scale_factor}")
+        i += 1
     
+    # Write scaling factors
     info_file=open(f"{args.output}/scaling_factors.txt", 'w')
     info_file.write(" ".join(scale_factors))
     info_file.flush()
     info_file.close()
 
     # Write the summary file
-    header = ['type', 'channel', 'q0', 'q0.1', 'q1', 'q25', 'q50', 'q75', 'q99', 'q99.9', 'q100', 'mean']
+    header = ['type', 'channel', 'q0', 'q0.1', 'q1', 'q25', 'q50', 'q75', 'q95', 'q99', 'q99.9', 'q99.99', 'q99.999', 'q99.9999', 'q100', 'mean']
     mean_file=open(f"{args.output}/intensity_summary.tsv", 'w')
     mean_file.write("\t".join(header))
     mean_file.write("\n")
     
-    for ch in range(len(channels)):
-        mean_file.write(f"mean_intensity\t{channels[ch]}\t")
+    for ch in range(len(indices)):
+        mean_file.write(f"mean_intensity\t{indices[ch]}\t")
         mean_file.write("\t".join([str(num) for num in stats_mean[ch]]))
         mean_file.write("\n")
         
-        mean_file.write(f"median_intensity\t{channels[ch]}\t")
+        mean_file.write(f"median_intensity\t{indices[ch]}\t")
         mean_file.write("\t".join([str(num) for num in stats_median[ch]]))
         mean_file.write("\n")
     
-        mean_file.write(f"max_intensity\t{channels[ch]}\t")
+        mean_file.write(f"max_intensity\t{indices[ch]}\t")
         mean_file.write("\t".join([str(num) for num in stats_max[ch]]))
         mean_file.write("\n")
     
     mean_file.flush()
     mean_file.close()
+    
+    log.info("Done")

bin/run_cellpose.py
@@ -196,7 +196,7 @@ class CellposeRunner():
             img = downscale_local_mean(img, (1, self.downsample, self.downsample, 1))
             log.info(f"Downsampled image to {img.shape}, running cellpose with d {self.diameter}")
         
-        # Normalize between 1st and 99th percentile
+        # Normalize between 1st and 99.99th percentile
         for channel in range(0,img.shape[3]):
             img[:,:,:,channel] = img[:,:,:,channel] - np.percentile(img[:,:,:,channel], 1)
             img[:,:,:,channel] = img[:,:,:,channel] / np.percentile(img[:,:,:,channel], 99.99)